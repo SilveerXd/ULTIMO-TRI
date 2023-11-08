8cae7f79cff9dfddafc8ec38b7ebde3f280966dd
João Marcos <113352648+SilveerXd@users.noreply.github.com>
2023-11-08 09:05:23 -0300

    Add files via upload

--- contas.js
+++ contas.js
@@ -4,23 +4,25 @@ const resp2 = document.querySelector ("#outResp2")
 
 let numContas = 0
 let valTotal = 0
-let resposta = 0
+let resposta = ""
 
 frm.addEventListener("submit",  (e) => {
- e.preventDefault()
+ 
+    e.preventDefault()
   
- const descricao = fmr.inDescricao.value
+ const descricao = frm.inDescricao.value
  const valor = Number(frm.inValor.value)
 
- numcontas
+ numContas++
  valTotal = valTotal + valor
 
- resposta = resposta = descricao = " R$: " + valor.toFixed(2) + "\n"
+ resposta = resposta + descricao + " R$: " + valor.toFixed(2) + "\n"
 
  resp1.innerText = `${resposta}-----------------------`
- resp2.innerText = `${numcontas} conta(s) - Total R$: ${valTotal.toFixed(2)}`
+ resp2.innerText = `${numContas} Conta(s) - Total R$: ${valTotal.toFixed(2)}`
 
  frm.inDescricao.value = ""
  frm.inValor.value = ""
  frm.inDescricao.focus()
+ 
 })
